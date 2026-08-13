16c3d7687941c69c5c107f17b8de83a8ca28682c
nssrathod <nssrathod2005@gmail.com>
2026-08-13 15:38:47 +0530

    Time: 15 ms (32.22%) | Memory: 46.7 MB (42.49%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -2,12 +2,8 @@ class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         Map<Character, Integer> map = new HashMap<>();
         for(char ch: magazine.toCharArray()){
-            if(map.containsKey(ch)){
-                 map.put(ch, map.get(ch) + 1);
-            }
-            else{
-                map.put(ch, 1);
-            }
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            
         }
         for(char ch: ransomNote.toCharArray()){
             if(!map.containsKey(ch)){
