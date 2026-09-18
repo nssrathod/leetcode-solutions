c420c2f6ab6e036ff589a8e5b97bc797f01258c4
nssrathod <nssrathod2005@gmail.com>
2026-09-18 14:07:59 +0530

    Time: 1 ms (98.05%) | Memory: 44.5 MB (39.59%) - LeetSync

---
+++ 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+
+        if (root == null) {
+            return res;
+        }
+
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                TreeNode node = queue.poll();
+
+                level.add(node.val);
+
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+
+            // Add each level at the beginning
+            res.add(level);
+        }
+        Collections.reverse(res);
+        return res;
+    }
+}
